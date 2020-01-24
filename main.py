219a5de4e1d826cafaf517b3fa608e20db6bf1aa
ekaterinaAP <ekaterinagjkzrjdf@yandex.ru>
2020-01-24 19:15:41 +0000

    Учёт повторений

--- main.py
+++ main.py
@@ -42,27 +42,30 @@ with open("recipes.txt") as recipe:
     recipe.seek(0)
 
     def get_shop_list_by_dishes(dishes, person_count):
-        shop_list = {}
+        shop_dict = {}
 
         while True:
             dish = recipe.readline().strip()        
             if not dish:
                 break
 
-            number = int(recipe.readline().strip())            
-
-            count = 0
-            while count < number:
+            number = int(recipe.readline().strip())
+            
+            for ingredient in range(number):
                 ingredient = recipe.readline().strip()
-                ingredient_list = ingredient.split("|")            
+                ingredient_list = ingredient.split("|")                
+                repetition = dishes.count(dish)
+                quantity = int(ingredient_list[1]) * person * repetition                
                 if dish in dishes:
-                    shop_list[ingredient_list[0]] = {"measure": ingredient_list[2], "quantity": int(ingredient_list[1])*person_count}
-
-                count +=1
-
+                    if ingredient_list[0] not in shop_dict:
+                        shop_dict[ingredient_list[0]] = {"measure": ingredient_list[2], "quantity": quantity}
+                    else:                        
+                        quantity += int(shop_dict[ingredient_list[0]]["quantity"])
+                        shop_dict[ingredient_list[0]] = {"measure": ingredient_list[2], "quantity": quantity}            
+                                       
             recipe.readline().strip()
 
-        return shop_list
+        return shop_dict
 
     try:
         food = input("Перечислите блюда через запятую, которые нужно приготовить: ")
@@ -73,13 +76,13 @@ with open("recipes.txt") as recipe:
         print("Вы ошиблись при вводе")
         print("Состав блюд для Омлета,Утки по-пекински,Запеченного картофеля,Фахитос на одну порцию")
         food = "Омлет,Утка по-пекински,Запеченый картофель,Фахитос"
-        person = 1
+        person = 1    
     
-    dishes_list = food.split(",")      
-    
-    shop_list = get_shop_list_by_dishes(dishes_list, person)
-    for ingredient in shop_list:
-        print(ingredient, shop_list[ingredient])
+    dishes_list = food.split(",")           
+        
+    shop_dict = get_shop_list_by_dishes(dishes_list, person)
+    for ingredient in shop_dict:
+        print(ingredient, shop_dict[ingredient])
       
 
         
